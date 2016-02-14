c05b4deb7904fd96595fd5a25dc11d8c99f770d0
Damien Pobel <dp@ez.no>
2016-02-14 14:44:51 +0100

    Added Greasemonkey script skeleton

---
+++ addvideo.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        github add youtube
+// @namespace   damien@pobel.fr
+// @include     https://github.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+console.log("Hey");
