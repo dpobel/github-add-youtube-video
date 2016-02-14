47d1347052c94ca283a8885186437ed45bdb7643
Damien Pobel <dp@ez.no>
2016-02-14 14:53:52 +0100

    Make sure the script is executed everywhere on github

--- addvideo.user.js
+++ addvideo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        github add youtube
 // @namespace   damien@pobel.fr
-// @include     https://github.com/
+// @include     https://github.com/*
 // @version     1
 // @grant       none
 // ==/UserScript==
